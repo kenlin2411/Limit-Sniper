6c7441d96b08161cde9bd64a160bedf4294dbce3
TsarBuig <70858574+tsarbuig@users.noreply.github.com>
2022-02-01 01:13:04 +0100

    - Re-used original Sniper function - Controls at bot launch - Fix if BUYAMOUNTINBASE is empty - Display improvements for price = 99999 - Fix for benchmark with ws:

--- sniper.py
+++ sniper.py
@@ -1115,7 +1115,7 @@ printt("************************************************************************
 
 # Check for version
 #
-version = '1.0.1'
+version = '1.0.2'
 printt("YOUR BOT IS CURRENTLY RUNNING VERSION ", version, write_to_log=True)
 check_release()
 
