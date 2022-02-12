02df36651d34f99744f3cf97c5d90aae206f036c
TsarBuig <70858574+tsarbuig@users.noreply.github.com>
2022-02-13 00:07:43 +0100

    v1.1.2

--- sniper.py
+++ sniper.py
@@ -1119,7 +1119,7 @@ printt("************************************************************************
 
 # Check for version
 #
-version = '1.1.1'
+version = '1.1.2'
 printt("YOUR BOT IS CURRENTLY RUNNING VERSION ", version, write_to_log=True)
 check_release()
 
